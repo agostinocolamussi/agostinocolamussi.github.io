8f236590858a154bc04c637fee513c127b03d90d
Agostino Colamussi <agostinocolamussi@gmail.com>
2020-12-15 20:26:32 +0100

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,24 @@
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-});
-buttonInstall.addEventListener('click', (e) => {
-  // Hide the app provided install promotion
-  hideMyInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
+var cacheName = 'pwa-agostinocolamussi.github.io';
+var filesToCache = [
+'/',
+'/index.html',
+'/css/stylesheet.css',
+'/images/pwa-logo.svg',
+'/js/core.js'
+]; 
+/* Avvia il Service Worker e Memorizza il contenuto nella cache */
+self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open(cacheName).then(function(cache) {
+return cache.addAll(filesToCache);
+})
+);
+}); 
+/* Serve i Contenuti Memorizzati quando sei Offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+  caches.match(e.request).then(function(response) {
+  return response || fetch(e.request);
+  })
+  );
   });
-});
-window.addEventListener('appinstalled', (evt) => {
-  // Log install to analytics
-  console.log('INSTALL: Success');
-});
-window.addEventListener('DOMContentLoaded', () => {
-  let displayMode = 'browser tab';
-  if (navigator.standalone) {
-    displayMode = 'standalone-ios';
-  }
-  if (window.matchMedia('(display-mode: standalone)').matches) {
-    displayMode = 'standalone';
-  }
-  // Log launch display mode to analytics
-  console.log('DISPLAY_MODE_LAUNCH:', displayMode);
-});
-window.addEventListener('DOMContentLoaded', () => {
-  window.matchMedia('(display-mode: standalone)').addListener((evt) => {
-    let displayMode = 'browser tab';
-    if (evt.matches) {
-      displayMode = 'standalone';
-    }
-    // Log display mode change to analytics
-    console.log('DISPLAY_MODE_CHANGED', displayMode);
-  });
-});
