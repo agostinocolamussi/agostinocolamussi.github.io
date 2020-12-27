0c4e4bba7e6e11927830d83a7eb22d1b4225e63d
Agostino Colamussi <agostinocolamussi@gmail.com>
2020-12-27 01:32:30 +0100

    service worker

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,11 @@ var cacheName = 'pwa-agostinocolamussi.github.io';
 var filesToCache = [
 '/',
 '/index.html',
-'/css/stylesheet.css',
-'/images/pwa-logo.svg',
-'/js/core.js'
+'/contenuti/cache/min/1/1d1ee75c5cc7ed8d6de45c7b4972eeab.css',
+'/contenuti/cache/min/1/189ffae5acefc239410d698e61e0ad2e.js',
+'/contenuti/cache/min/1/bc0a64c3a966c0b6843a9753a0438227.css',
+'/contenuti/cache/min/1/c51b64b9e13c6db0ef718482620d736f.js',
+'/favicon.ico'
 ]; 
 /* Avvia il Service Worker e Memorizza il contenuto nella cache */
 self.addEventListener('install', function(e) {
